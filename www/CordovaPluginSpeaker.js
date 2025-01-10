cc097a592a954be5797d12ea37c27ee96fd40cb8
PeOmniBk <PeOmniBk@path-solutions.com>
2025-01-11 00:42:46 +0200

    remove common function

--- www/CordovaPluginSpeaker.js
+++ www/CordovaPluginSpeaker.js
@@ -1,11 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.unifiedMethod = function (arg0, arg1, success, error) {
-    if (cordova.platformId === 'android') {
-        exec(success, error, 'CordovaPluginSpeaker', 'coolMethod', [arg0, arg1]);
-    } else if (cordova.platformId === 'ios') {
-        exec(success, error, 'CordovaPluginIos', 'coolMethodd', [arg0]);
-    } else {
-        error('Unsupported platform');
-    }
+exports.coolMethod = function (arg0, arg1, success, error) {
+    exec(success, error, 'CordovaPluginSpeaker', 'coolMethod', [arg0, arg1]);
+};
+
+exports.coolMethodd = function (arg0, success, error) {
+    exec(success, error, 'CordovaPluginIos', 'coolMethodd', [arg0]);
 };
